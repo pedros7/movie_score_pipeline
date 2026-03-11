a8f17a650023ecae81577fac97fecf932481739f
pedros7 <pedrosoragujobs@gmail.com>
2026-03-11 18:53:09 +0100

    TASK-4 Improved baseprovider class

--- src/providers/base_provider.py
+++ src/providers/base_provider.py
@@ -1,6 +1,11 @@
 import abc
+import pandas as pd
 
 class BaseProvider(metaclass=abc.ABCMeta):
     @abc.abstractmethod
-    def fetch(self, url):
+    def fetch(self, url: str):
+        pass
+
+    @abc.abstractmethod
+    def transform(self, data: pd.DataFrame):
         pass
