4e0e94375c1bc74cc05bbd3ae0d2e38cda60bf18
pedros7 <pedrosoragujobs@gmail.com>
2026-03-11 18:54:00 +0100

    TASK-4 Created mock criticagg provider

--- src/providers/critic_agg.py
+++ src/providers/critic_agg.py
@@ -1,4 +1,6 @@
 from base_provider import BaseProvider
+from ...models.movie_score_model import *
+import pandas as pd
 
 class CriticAggProvider(BaseProvider):
     def __init__(self):
@@ -6,4 +8,7 @@ class CriticAggProvider(BaseProvider):
         self.name = "critic_agg"
 
     def fetch(self, url):
+        return "Function to be developed"
+
+    def transform(self, df: pd.DataFrame):
         return "Function to be developed"
