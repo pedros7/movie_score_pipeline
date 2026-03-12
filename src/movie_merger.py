fcd915a5621efe09d07323335674ef40ca4bdc06
pedros7 <pedrosoragujobs@gmail.com>
2026-03-12 12:07:39 +0100

    TASK-7 created base merging fucntion

---
+++ src/movie_merger.py
@@ -0,0 +1,8 @@
+from typing import List
+from ..models.movie_score_model import MovieScore
+
+
+def merge_movie_lists(*provider_lists: List[MovieScore]) -> List[MovieScore]:
+    # TODO: Develop the function, it should receive the data from different providers and merge it into a single list of
+    #  moviescore items
+    pass
