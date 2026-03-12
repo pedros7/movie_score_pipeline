5c977c95ed29a86c013549ccbb9e93e43ee1d1a5
pedros7 <pedrosoragujobs@gmail.com>
2026-03-12 17:08:30 +0100

    TASK-8 Implemented solid pipeline tests

--- test/test_pipeline.py
+++ test/test_pipeline.py
@@ -2,11 +2,6 @@ import pytest
 from src.pipeline import MoviePipeline
 from test_critic_agg import URL_TEST_DATA_PROVIDER1
 from test_audience_pulse import URL_TEST_DATA_PROVIDER2
-from test_box_office_metrics import (
-    URL_TEST_DATA_PROVIDER3_DOMESTIC,
-    URL_TEST_DATA_PROVIDER3_FINANCIALS,
-    URL_TEST_DATA_PROVIDER3_INTERNATIONAL,
-)
 
 
 def test_pipeline_runs(
@@ -22,17 +17,15 @@ def test_pipeline_runs(
 
     result = pipeline.run()
 
-    assert len(result) > 0
+    assert len(result) == 3
 
 
 def test_pipeline_merges_data(pipeline):
     movies = pipeline.run()
-
     movie = movies[0]
 
     assert movie.title is not None
     assert movie.year is not None
-
     assert (
         movie.critic_score_percentage
         or movie.audience_average_score
@@ -41,7 +34,6 @@ def test_pipeline_merges_data(pipeline):
 
 
 def test_pipeline_accepts_variable_providers(test_critic_agg_provider):
-
     pipeline = MoviePipeline(
         providers=[
             (test_critic_agg_provider, URL_TEST_DATA_PROVIDER1),
@@ -50,4 +42,4 @@ def test_pipeline_accepts_variable_providers(test_critic_agg_provider):
 
     result = pipeline.run()
 
-    assert len(result) > 0
+    assert len(result) == 3
