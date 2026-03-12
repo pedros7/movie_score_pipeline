e711347dbcb9e6a875eac3afd85d964f21716acb
pedros7 <pedrosoragujobs@gmail.com>
2026-03-12 12:31:50 +0100

    TASK-7 Added new fixtures + skeletal test ideas

--- test/test_movie_merger.py
+++ test/test_movie_merger.py
@@ -2,6 +2,8 @@ import pytest
 from models.movie_score_model import MovieScore
 from src.movie_merger import movie_merger
 
+""" Fixtures """
+
 
 @pytest.fixture
 def critic_agg_movie():
@@ -35,3 +37,33 @@ def box_office_metrics_movie():
         production_budget_usd=4000000,
         marketing_spend_usd=2000000,
     )
+
+@pytest.fixture
+def audience_pulse_movie_alt():
+    return MovieScore(
+        title="Eternal Sunshine of the Spotless Mind",
+        year=2004,
+        audience_average_score=8.4,
+        total_audience_ratings=1100000,
+        domestic_box_office_gross=34400000,
+    )
+
+
+
+""" Unit tests """
+
+
+def test_merge_combines_fields(critic_agg_movie, audience_pulse_movie):
+    pass
+
+def test_merge_combines_different_movies(critic_agg_movie, audience_pulse_movie):
+    pass
+
+def test_merge_field_preservation(critic_agg_movie, audience_pulse_movie):
+    pass
+
+def test_merge_handles_empty_list(critic_agg_movie, audience_pulse_movie):
+    pass
+
+def test_merge_handles_three_providers(critic_agg_movie, audience_pulse_movie, box_office_metrics_movie):
+    pass
