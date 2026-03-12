eb387e978f5562ff8d5b8e4eef2a817c47f8db51
pedros7 <pedrosoragujobs@gmail.com>
2026-03-12 12:20:06 +0100

    TASK-7 Simple rename of te method

--- src/movie_merger.py
+++ src/movie_merger.py
@@ -2,7 +2,7 @@ from typing import List
 from ..models.movie_score_model import MovieScore
 
 
-def merge_movie_lists(*provider_lists: List[MovieScore]) -> List[MovieScore]:
+def movie_merger(*provider_lists: List[MovieScore]) -> List[MovieScore]:
     # TODO: Develop the function, it should receive the data from different providers and merge it into a single list of
     #  moviescore items
     pass
